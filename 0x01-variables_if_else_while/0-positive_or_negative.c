7ec2e9d76321c3a14583ca2a91ad270753dc7682
kewell0 <Owatahcaleb@gmail.com>
2022-03-11 18:14:35 -0800

    task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,8 @@
 /**
  *main - Entry
  *
- * Description: determine if number is positive or negative
+ * Description: program will assign a random number to the variable n each time
+ * it is executed
  * Return: 0
  */
 
