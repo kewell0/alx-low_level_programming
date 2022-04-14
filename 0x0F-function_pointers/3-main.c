bd0446d919a1a415beffdb6f615fbbefa455832e
kewell0 <63118280+kewell0@users.noreply.github.com>
2022-04-14 06:21:06 +0100

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,36 +1,44 @@
 #include "3-calc.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
- * main - performs simple operations (calculator)
- * @argc: number of arguments
- * @argv: array of arguments
- * Return: 0 (Always)
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: int
  */
 
 int main(int argc, char *argv[])
 {
-	int a, b, result;
-	int (*op)(int, int);
+	char *sign;
+	char operator;
+	int a, b, answer;
+	int (*ptr)(int, int);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-
-	op = get_op_func(argv[2]);
-	if (op == NULL)
+	operator = argv[2][0];
+	if ((operator != '*' && operator != '+' && operator != '-'
+	     && operator != '/'  && operator != '%') || argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
-	result = op(a, b);
-
-	printf("%d\n", result);
-
+	if ((operator == '/' || operator == '%') && b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	sign = argv[2];
+	ptr = get_op_func(sign);
+	answer = (*ptr)(a, b);
+	printf("%d\n", answer);
 	return (0);
 }
