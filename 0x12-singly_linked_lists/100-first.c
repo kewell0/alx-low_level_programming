16197589e7d54a6c3999e839be47dfbdeb7d8619
kewell0 <63118280+kewell0@users.noreply.github.com>
2022-04-22 01:17:48 +0100

    Create 100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void p_before_main(void) __attribute__ ((constructor));
+/**
+ * p_before_main - prints before main function
+ */
+void p_before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n I bore my house upon my back!\n");
+}
