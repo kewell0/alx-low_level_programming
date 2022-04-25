b9ec65a98c3f05ed9aafaa919e016703a7dafc4c
kewell0 <63118280+kewell0@users.noreply.github.com>
2022-04-25 20:25:22 +0100

    Update 103-find_loop.c

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,30 +1,30 @@
 #include "lists.h"
 
 /**
- * find_listint_loop - finds the first member of a linked list
- * @head: pointer to the head
- * Return: number of nodes
+ * find_listint_loop - finds the loop in a linked list
+ * @head: head of linked list
+ * Return: address of the node where the loop starts
  */
+
 listint_t *find_listint_loop(listint_t *head)
 {
-	listint_t *slow = head;
-	listint_t *fast = head;
+	listint_t *slow, *fast;
 
-	while (slow && fast && fast->next)
+	slow = head;
+	fast = head;
+	while (head != NULL)
 	{
-		slow = slow->next;
-		fast = fast->next->next;
 		if (slow == fast)
 		{
-		       	slow = head;
-			while (slow->next != fast->next)
+			while (head != NULL)
 			{
+				if (slow == fast)
+					return (slow);
 				slow = slow->next;
 				fast = fast->next;
 			}
-			return (slow);
 		}
+		slow = slow->next;
+		fast = fast->next->next;
 	}
-
-	return (NULL);
 }
