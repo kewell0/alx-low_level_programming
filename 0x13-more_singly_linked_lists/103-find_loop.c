b81a9adea8dcace5ca4dae81f3538498c883e04a
kewell0 <63118280+kewell0@users.noreply.github.com>
2022-04-27 11:41:45 +0100

    Update 103-find_loop.c

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -15,21 +15,21 @@ listint_t *find_listint_loop(listint_t *head)
     if (!head)
         return (NULL);
 
-        while (slow && fast && fast->next)
+    while (slow && fast && fast->next)
+    {
+        fast = fast->next->next;
+        slow = slow->next;
+        if (fast == slow)
         {
-            fast = fast->next->next;
-            slow = slow->next;
-            if (fast == slow)
+            slow = head;
+            while (slow != fast)
             {
-                slow = head;
-                while (slow != fast)
-                {
-                    slow = slow->next;
-                    fast = fast->next;
-                }
-                return (fast);
+                slow = slow->next;
+                fast = fast->next;
             }
+            return (fast);
         }
+    }
 
     return (NULL);
 }
