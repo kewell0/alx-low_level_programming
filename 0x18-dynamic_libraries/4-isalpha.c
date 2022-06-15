bada8df52e9a9b69a61adc0596df5f1934e0190c
Caleb Owatah <Owatahcaleb@gmail.com>
2022-06-15 04:56:48 -0700

    dynamic.so

--- 0x18-dynamic_libraries/4-isalpha.c
+++ 0x18-dynamic_libraries/4-isalpha.c
@@ -1,33 +1,21 @@
 #include "main.h"
 
 /**
- * _strpbrk - Search a string for any of a set of bytes.
+ * _isalpha - Search a string for any of a set of bytes.
  * @s: string
  * @accept: string to match
  * Return: Pointer to the byte in `s` that matches one of the bytes in `accept`
  * or NULL if no such byte is found
  */
 
-char *_strpbrk(char *s, char *accept)
+int _isalpha(int c)
 {
-	int i, j;
-	char *p;
-
-	i = 0;
-	while (s[i] != '\0')
+	if ((c >= 97 && c <= 123) || (c >= 65 && c <= 91))
 	{
-		j = 0;
-		while (accept[j] != '\0')
-		{
-			if (accept[j] == s[i])
-			{
-				p = &s[i];
-				return (p);
-			}
-			j++;
-		}
-		i++;
+		return (1);
 	}
-
-	return (0);
+	else
+	{
+		return (0);
+	}
 }
