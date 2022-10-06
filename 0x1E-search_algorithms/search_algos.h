a3ec9c12e797fba76c0cfc1ed65351e3910106ab
CALEB <owatahcaleb@gmail.com>
2022-10-06 02:47:03 +0100

    search

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,11 @@
+#ifndef __SEARCH_ALGOS_H__
+#define __SEARCH_ALGOS_H__
+
+/*==== My function Prototypes ====*/
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+#endif
