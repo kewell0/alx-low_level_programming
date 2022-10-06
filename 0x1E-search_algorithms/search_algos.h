514e332a61fccaa3cb9a63d07a621d739082eed8
CALEB <owatahcaleb@gmail.com>
2022-10-06 03:10:43 +0100

    Update search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,7 +1,8 @@
 #ifndef __SEARCH_ALGOS_H__
 #define __SEARCH_ALGOS_H__
 
-/*==== My function Prototypes ====*/
+/*==== Prototypes ====*/
+int linear_search(int *array, size_t size, int value);
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
